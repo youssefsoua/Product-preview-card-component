e3213111596ec865cdbe9fb26d0d7bea42166d8d
youssefsoua <youssefsoua94@gmail.com>
2023-08-21 14:49:12 +0100

    styles enhancements

--- src/App.tsx
+++ src/App.tsx
@@ -20,21 +20,21 @@ function App() {
             <p className="text-xs font-medium uppercase tracking-[0.35em] text-dark-grayish-blue lg:tracking-[0.4em]">
               Perfume
             </p>
-            <h1 className="py-3 font-Fraunces text-[32px] font-bold leading-8 text-very-dark-blue lg:py-6">
+            <h1 className="py-3 font-Fraunces text-[32px] font-bold leading-8 text-very-dark-blue lg:py-5">
               Gabrielle Essence Eau De Parfum
             </h1>
           </header>
-          <p className="py-1 font-medium leading-[1.65] text-dark-grayish-blue lg:py-0">
+          <p className="py-1 font-medium leading-[1.65] text-dark-grayish-blue lg:py-1.5">
             A floral, solar and voluptuous interpretation composed by Olivier
             Polge, Perfumer-Creator for the House of CHANEL.
           </p>
-          <p className="flex items-center py-4 font-Fraunces text-3xl text-dark-cyan lg:py-6">
+          <p className="flex items-center py-4 font-Fraunces text-[32px] leading-8 text-dark-cyan lg:py-6">
             $149.99
             <s className="px-5 font-Montserrat text-[13px] font-medium text-dark-grayish-blue">
               $169.99
             </s>
           </p>
-          <button className="flex w-full flex-row items-center justify-center rounded-lg bg-dark-cyan py-3.5 font-medium text-white hover:bg-green-950">
+          <button className="flex w-full flex-row items-center justify-center rounded-lg bg-dark-cyan py-3.5 font-bold text-white hover:bg-green-950">
             <img src="./icon-cart.svg" alt="cart image" className="px-2.5" />
             Add to Cart
           </button>
